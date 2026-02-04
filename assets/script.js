d3d4a26a4aa8a4c96ad8aa6431c24ad2ea35d08e
Meli-Projets <melisande77320@gmail.com>
2026-02-04 16:01:20 +0100

    Étape 2 : Ajout des Event Listeners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,19 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Déclaration des constantes
+const arrowRight = document.querySelector(".arrow_right");
+const arrowLeft = document.querySelector(".arrow_left");
+
+// Ajout d'un event listener sur la flèche droite
+arrowRight.addEventListener("click", function (e) {
+  console.log(this.className); // Affiche le nom de la classe
+  console.log(e.currentTarget === this); // Vérifie que l'élément cliqué est celui qui écoute l'événement (true)
+});
+
+// Ajout d'un event listener sur la flèche gauche
+arrowLeft.addEventListener("click", function (e) {
+  console.log(this.className);
+  console.log(e.currentTarget === this);
+});
