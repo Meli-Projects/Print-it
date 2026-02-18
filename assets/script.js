9b0f6a1512b8db461ddcfd4bd6421606d7c25660
Meli-Projets <melisande77320@gmail.com>
2026-02-18 12:41:17 +0100

    Suppression des commentaires supplémentaires et console.log

--- assets/script.js
+++ assets/script.js
@@ -23,49 +23,41 @@ let indexTable = 0;
 // Déclaration des constantes
 const arrowRight = document.querySelector(".arrow_right");
 const arrowLeft = document.querySelector(".arrow_left");
-const dots = document.querySelector(".dots"); // Sélection de la div .dots qui servira de conteneur aux bullet points (élément parent)
-const bannerImg = document.querySelector(".banner-img"); //Sélection des images bannière
-const bannerText = document.querySelector("#banner p"); //Sélection des textes bannière
+const dots = document.querySelector(".dots"); 
+const bannerImg = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p"); 
 
 
 // fonction qui met à jour l'affichage (image + texte) bannière
 function updateSlide() {
   bannerImg.setAttribute( 
-    "src",
-    "./assets/images/slideshow/" + slides[indexTable].image
+    "src","./assets/images/slideshow/" + slides[indexTable].image
   );
   
   bannerText.innerHTML = slides[indexTable].tagLine;
 
 }
 // fonction qui met à jour l'affichage (dot) bannière
-function updateDot(indexTable) {
+function updateDot(index) {
   const allDots = document.querySelectorAll(".dot");
   allDots.forEach((dot) => dot.classList.remove("dot_selected"));
-  allDots[indexTable].classList.add("dot_selected");
+  allDots[index].classList.add("dot_selected");
 }
 
 // Gestion des bulles (bullet du slider)
-// Début de la boucle pour répéter la création et l'ajout des bulles en fonction du nombre de slides
 for (let i = 0; i < slides.length; i++) {
-	const newDot = document.createElement("div"); // Création d'un nouvel élément div qui représentera une bulle (dot)
-	newDot.className = "dot"; // Ajout de la classe CSS "dot" à la bulle pour qu'elle soit stylisée
+	const newDot = document.createElement("div"); 
+	newDot.className = "dot"; 
 	
-	// Ajout du style "sélectionné" sur le premier point (slide affichée au chargement)
 	if (i === 0) {
-		newDot.classList.add("dot_selected") // Ajoute la class spécifique au point actif
+		newDot.classList.add("dot_selected") 
 }	
-	dots.appendChild(newDot); // Insertion de la bulle dans le conteneur .dots
+	dots.appendChild(newDot); 
 }
 
-
 // Gestion des flêches
-// Ajout d'un event listener sur la flèche droite
 arrowRight.addEventListener("click", function (e) {
-  console.log(this.className); // Affiche le nom de la classe
-  console.log(e.currentTarget === this); // Vérifie que l'élément cliqué est celui qui écoute l'événement (true)
-  
-    //Boucle infinie : si on est sur la dernière slide, on revient à la première
+
   	if (indexTable === slides.length - 1) {
   	indexTable = 0;
 
@@ -78,10 +70,7 @@ arrowRight.addEventListener("click", function (e) {
 
 // Ajout d'un event listener sur la flèche gauche
 arrowLeft.addEventListener("click", function (e) {
-  console.log(this.className);
-  console.log(e.currentTarget === this);
 
-	// Boucle infinie : on recule d'une slide, et si on passe sous 0 on va à la dernière
 	indexTable--;
 
 	if (indexTable < 0) {
@@ -94,6 +83,3 @@ arrowLeft.addEventListener("click", function (e) {
 
 updateSlide();
 updateDot(indexTable);
-
-
-
